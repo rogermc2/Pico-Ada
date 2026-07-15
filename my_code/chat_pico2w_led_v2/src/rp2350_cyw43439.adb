ab7936fe69ee71c3b47d294513923666b0d2d7c0
rogermc2 <rogermc@iinet.net.au>
2026-07-15 20:30:38 +1000

    Deleted Registers file

--- my_code/chat_pico2w_led_v2/src/rp2350_cyw43439.adb
+++ my_code/chat_pico2w_led_v2/src/rp2350_cyw43439.adb
@@ -1,19 +1,25 @@
 
+with RP2350; use RP2350;
 with RP2350.IO_BANK0;
 with System;
-
-with Registers; use Registers;
 
 with RP2350.SIO; use RP2350.SIO;
 
 package body RP2350_CYW43439 is
 
+   -- Bitmasks
+   Mask_REG_ON   : constant uint32 := 16#0080_0000#;
+   Mask_DATA     : constant uint32 := 16#0100_0000#;
+   Mask_CS       : constant uint32 := 16#0200_0000#;
+   Mask_CLK      : constant uint32 := 16#2000_0000#;
+   All_Pins_Mask : constant uint32 := 16#2380_0000#;
+
 function Check_Chip_Communication return Unsigned_32 is
-     Read_Header : constant Unsigned_32 := Shift_Left(16#0014#, 11) or 4;
-   Periph      : SIO_Peripheral;
+   Read_Header : constant Unsigned_32 := Shift_Left(16#0014#, 11) or 4;
+   Wifi        : SIO_Peripheral;
    Result      : Unsigned_32 := 0;
 begin
-   Periph.GPIO_Out_Clr := Mask_CS;
+   Wifi.GPIO_Out_Clr := Mask_CS;
    -- Send Read Request Header
    Write_gSPI_Word32 (Read_Header);
 
@@ -26,7 +32,8 @@ begin
              Shift_Left (Unsigned_32 (Read_gSPI_Byte), 8)  or
              Unsigned_32 (Read_gSPI_Byte);
 
-   Periph.GPIO_OUT_SET := Mask_CS;
+   Wifi.GPIO_OUT_SET := Mask_CS;
+
    return Result;
 
    end Check_Chip_Communication;
@@ -42,7 +49,7 @@ begin
       Wake_Header : constant Unsigned_32 :=
         16#8000_0000# or 16#4000_0000# or
          Shift_Left (16#1800_00A2#, 11) or 1;
-      Periph : SIO_Peripheral;
+      Wifi : SIO_Peripheral;
    begin
       -- 1. Route pins to SIO function (Function 5 on RP2350)
       GPIO23_Ctrl.FUNCSEL := IO_BANK0.siob_proc_23;
@@ -51,47 +58,46 @@ begin
       GPIO29_Ctrl.FUNCSEL := IO_BANK0.siob_proc_29; -- CLK
 
       -- 2. Configure default output directions
-      Periph.GPIO_OE_SET  :=  All_Pins_Mask;
+      Wifi.GPIO_OE_SET  :=  All_Pins_Mask;
 
       -- 3. Set idle state
-      Periph.GPIO_OUT_SET := Mask_CS or Mask_REG_ON;
-
-      Periph.GPIO_OUT_CLR := Mask_REG_ON;
+      Wifi.GPIO_OUT_SET := Mask_CS or Mask_REG_ON;
+      Wifi.GPIO_OUT_CLR := Mask_REG_ON;
       Wait (Milliseconds (50));
 
-      Periph.GPIO_OUT_SET :=  Mask_REG_ON;
+      Wifi.GPIO_OUT_SET :=  Mask_REG_ON;
       --  Wait for internal wireless boot ROM to execute
       Wait (Milliseconds (25));
-      Periph.GPIO_OUT_CLR := Mask_CS;
+      Wifi.GPIO_OUT_CLR := Mask_CS;
 
       Write_gSPI_Word32 (Wake_Header);
       Write_gSPI_Byte (2);  --  Request active HT internal clock
-      Periph.GPIO_OUT_SET := Mask_CS;
+      Wifi.GPIO_OUT_SET := Mask_CS;
 
       Wait (Milliseconds (100));
 
    end Initialize_gSPI;
 
    procedure Write_gSPI_Byte (Data : Unsigned_8) is
-      Periph  : SIO_Peripheral;
+      Wifi  : SIO_Peripheral;
       Temp    : Unsigned_8 := Data;
    begin
       -- Ensure host drives the shared data line
-      Periph.GPIO_OE_SET  :=  Mask_DATA;
+      Wifi.GPIO_OE_SET  :=  Mask_DATA;
 
       -- Send MSB First
       for Bit in 1 .. 8 loop
-         Periph.GPIO_OUT_CLR := Mask_CLK; -- Clock Low
+         Wifi.GPIO_OUT_CLR := Mask_CLK; -- Clock Low
          if (Temp and 16#80#) /= 0 then
-            Periph.GPIO_OUT_SET := Mask_DATA;
+            Wifi.GPIO_OUT_SET := Mask_DATA;
          else
-            Periph.GPIO_OUT_CLR := Mask_DATA;
+            Wifi.GPIO_OUT_CLR := Mask_DATA;
          end if;
 
          -- Brief delay matching CYW43439 timing constraints (up to 33MHz limit)
          Wait (Milliseconds (5));
          -- Clock High (CYW43439 samples on rising edge)
-         Periph.GPIO_OUT_SET := Mask_CLK;
+         Wifi.GPIO_OUT_SET := Mask_CLK;
          Temp := Shift_Left (Temp, 1);
          
          Wait (Milliseconds (5));
@@ -101,18 +107,18 @@ begin
 
    function Read_gSPI_Byte return Unsigned_8 is
       use RP2350;
-      Periph  : SIO_Peripheral;
-      Result  : Unsigned_8 := 0;
+      Wifi   : SIO_Peripheral;
+      Result : Unsigned_8 := 0;
    begin
       -- Relinquish host drive control so CYW43439 can transmit
-       Periph.GPIO_OE_CLR := Mask_DATA;
+       Wifi.GPIO_OE_CLR := Mask_DATA;
       for Bit in 1 .. 8 loop
-         Periph.GPIO_OUT_CLR := Mask_CLK; -- Clock Low
-         Periph.GPIO_OUT_SET := Mask_CLK; -- Clock High
+         Wifi.GPIO_OUT_CLR := Mask_CLK; -- Clock Low
+         Wifi.GPIO_OUT_SET := Mask_CLK; -- Clock High
          Result  := Shift_Left (Result, 1);
 
          -- Sample line after edge propagation delay
-         if (Periph.GPIO_IN and Mask_DATA) /= 0 then
+         if (Wifi.GPIO_IN and Mask_DATA) /= 0 then
             Result := Result or 16#01#;
          end if;
 
@@ -152,7 +158,7 @@ begin
                          Shift_Left (1, 26) or             -- Function 1 (Backplane)
                          Shift_Left (GPIO_Out_Addr, 11) or -- Target memory address
                          4;                                -- Length of payload (4 Bytes)
-      Periph         : SIO_Peripheral;                  
+      Wifi           : SIO_Peripheral;                  
       Payload_Value  : Unsigned_32 := 0;
    begin
       -- Determine payload state for WL_GPIO0
@@ -164,7 +170,7 @@ begin
 
       -- Execute the gSPI bus cycle transaction
        -- Assert Chip Select Low to begin transaction
-      Periph.GPIO_OUT_CLR  := Mask_CS;
+      Wifi.GPIO_OUT_CLR  := Mask_CS;
       Write_gSPI_Word32 (SPI_Header);    -- Stream Header over SPI line
      
       -- CRITICAL CRUX: The gSPI Turnaround Delay
@@ -174,7 +180,7 @@ begin
       Write_gSPI_Word32 (Payload_Value); -- Stream Data Payload over SPI line
       Wait (Milliseconds (5));
       -- Deassert Chip Select High to conclude transfer
-      Periph.GPIO_OUT_SET := Mask_CS;
+      Wifi.GPIO_OUT_SET := Mask_CS;
 
    end Set_Onboard_LED;
 
