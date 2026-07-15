6ac06163232473f8a5e441ac16920528849174ef
rogermc2 <rogermc@iinet.net.au>
2026-07-15 17:41:27 +1000

    clean more

--- my_code/chat_pico2w_led_v2/src/pico2w_led.adb
+++ my_code/chat_pico2w_led_v2/src/pico2w_led.adb
@@ -6,8 +6,7 @@ with Ada.Real_Time; use Ada.Real_Time;
 with RP2350_CYW43439; use RP2350_CYW43439;
 
 procedure Pico2w_LED is
-   LED_State   : Boolean := False;
-   Test_Val    : Unsigned_32;
+   Test_Val : Unsigned_32;
 begin
     -- Initialize timing-critical gSPI bus lines
    Initialize_gSPI;
@@ -16,11 +15,9 @@ begin
    loop
       Set_Onboard_LED (True);  -- Turn LED On
       Wait (Seconds (5));
-      --  for I in 1 .. 5_000_000 loop null; end loop; -- Rough spin delay
 
       Set_Onboard_LED (False); -- Turn LED Off
       Wait (Seconds (5));
-      --  for I in 1 .. 5_000_000 loop null; end loop; -- Rough spin delay
    end loop;
    
 end Pico2w_LED;
