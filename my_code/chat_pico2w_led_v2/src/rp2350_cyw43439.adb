c7f2ae305e0b294389a9a9eb670f2fba6d7002e7
rogermc2 <rogermc@iinet.net.au>
2026-07-22 14:09:22 +1000

    Replaced local periferal definitions with SVD ones

--- my_code/chat_pico2w_led_v2/src/rp2350_cyw43439.adb
+++ my_code/chat_pico2w_led_v2/src/rp2350_cyw43439.adb
@@ -4,7 +4,7 @@ with System;
 with RP2350; use RP2350;
 with RP2350.IO_BANK0;
 with RP2350.PADS_BANK0;
-with RP2350.SIO; use RP2350.SIO;
+with RP2350.SIO;
 
 package body RP2350_CYW43439 is
 
@@ -16,11 +16,11 @@ package body RP2350_CYW43439 is
    All_Pins_Mask : constant uint32 := 16#2380_0000#;
 
 function Check_Chip_Communication return Unsigned_32 is
+   use RP2350.SIO;
    Read_Header : constant Unsigned_32 := Shift_Left(16#0014#, 11) or 4;
-   Wifi        : SIO_Peripheral;
    Result      : Unsigned_32 := 0;
 begin
-   Wifi.GPIO_Out_Clr := Mask_CS;
+   SIO_Periph.GPIO_OUT_CLR := Mask_CS;
    -- Send Read Request Header
    Write_gSPI_Word32 (Read_Header);
 
@@ -33,7 +33,7 @@ begin
              Shift_Left (Unsigned_32 (Read_gSPI_Byte), 8)  or
              Unsigned_32 (Read_gSPI_Byte);
 
-   Wifi.GPIO_OUT_SET := Mask_CS;
+   SIO_Periph.GPIO_OUT_SET := Mask_CS;
 
    return Result;
 
@@ -41,57 +41,50 @@ begin
 
    procedure Initialize_gSPI is
       use RP2350;
-      -- Control registers for mapping functions
-      GPIO23_Ctrl : IO_BANK0.GPIO23_CTRL_Register;
-      GPIO24_Ctrl : IO_BANK0.GPIO24_CTRL_Register;
-      GPIO25_Ctrl : IO_BANK0.GPIO25_CTRL_Register;
-      GPIO29_Ctrl : IO_BANK0.GPIO29_CTRL_Register;
-
-      GPIO24_Periph : PADS_BANK0.PADS_BANK0_Peripheral;
-      GPIO25_Periph : PADS_BANK0.PADS_BANK0_Peripheral;
-      GPIO29_Periph : PADS_BANK0.PADS_BANK0_Peripheral;
-      Wifi          : SIO_Peripheral;
+      use RP2350.IO_BANK0;
+      use RP2350.SIO;
       Wake_Header   : constant Unsigned_32 :=
       16#8000_0000# or Shift_Left(16#00A2#, 11) or 1;
    begin
       -- 1. Route pins to SIO function (Function 5 on RP2350)
-      GPIO23_Ctrl.FUNCSEL := IO_BANK0.siob_proc_23;
-      GPIO24_Ctrl.FUNCSEL := IO_BANK0.siob_proc_24; -- DATA
-      GPIO25_Ctrl.FUNCSEL := IO_BANK0.siob_proc_25; -- CS
-      GPIO29_Ctrl.FUNCSEL := IO_BANK0.siob_proc_29; -- CLK
+      IO_BANK0_Periph.GPIO23_CTRL.FUNCSEL := IO_BANK0.siob_proc_23;
+      IO_BANK0_Periph.GPIO24_CTRL.FUNCSEL := IO_BANK0.siob_proc_24;
+      IO_BANK0_Periph.GPIO25_CTRL.FUNCSEL := IO_BANK0.siob_proc_25;
+      IO_BANK0_Periph.GPIO29_CTRL.FUNCSEL := IO_BANK0.siob_proc_29;
 
       -- 2.  Configure the Input Enable (IE) using SVD PADS types
       -- Bit 6 = IE, Bit 3 = PUE (Pull-Up), Bits 4-5 = Drive Strength (12mA)
-      GPIO24_Periph.GPIO24.IE := 1;
-      GPIO24_Periph.GPIO24.PUE := 1;
-      GPIO24_Periph.GPIO24.DRIVE := PADS_BANK0.Val_12mA;
+      
+      PADS_BANK0.PADS_BANK0_Periph.GPIO24.IE := 1;
+      PADS_BANK0.PADS_BANK0_Periph.GPIO24.PUE := 1;
+      PADS_BANK0.PADS_BANK0_Periph.GPIO24.DRIVE := PADS_BANK0.Val_12mA;
 
-      GPIO24_Periph.GPIO25 := (0, 0, 0, 0, PADS_BANK0.Val_12mA, 0, 0, 0, 0);
-      GPIO24_Periph.GPIO29 := (0, 0, 0, 0, PADS_BANK0.Val_12mA, 0, 0, 0, 0);
+      PADS_BANK0.PADS_BANK0_Periph.GPIO25 := (0, 0, 0, 0, PADS_BANK0.Val_12mA, 0, 0, 0, 0);
+      PADS_BANK0.PADS_BANK0_Periph.GPIO29 := (0, 0, 0, 0, PADS_BANK0.Val_12mA, 0, 0, 0, 0);
 
       -- 3. Configure default output directions and isolate bus with CS high
-      Wifi.GPIO_OE_SET  :=  All_Pins_Mask;    --  0x23800000
+      SIO_Periph.GPIO_OE_SET  :=  All_Pins_Mask;    --  0x23800000
 
       -- 4. Set idle state, Cycle physical Power to the CYW43439
-      Wifi.GPIO_OUT_SET := Mask_CS or Mask_REG_ON;  --   0x2800000
+      SIO_Periph.GPIO_OUT_SET := Mask_CS or Mask_REG_ON;  --   0x2800000
       -- 5. Cycle physical hardware power to CYW43439
-      Wifi.GPIO_OUT_CLR := Mask_REG_ON;  --  0x800000
+      SIO_Periph.GPIO_OUT_CLR := Mask_REG_ON;  --  0x800000
       Wait (Milliseconds (50));
 
-      Wifi.GPIO_OUT_SET :=  Mask_REG_ON;  --  0x800000
+      SIO_Periph.GPIO_OUT_SET :=  Mask_REG_ON;  --  0x800000
       --  Wait for internal wireless boot ROM to execute
       Wait (Milliseconds (250));
 
       -- 6. Execute clock wake frame over the bus
-      Wifi.GPIO_OUT_CLR := Mask_CS;  --  0x2000000
+      SIO_Periph.GPIO_OUT_CLR := Mask_CS;  --  0x2000000
       Write_gSPI_Word32 (Wake_Header);
       Write_gSPI_Byte (2);  --  Request active HT internal clock
-      Wifi.GPIO_OUT_SET := Mask_CS;  --  0x2000000
+      SIO_Periph.GPIO_OUT_SET := Mask_CS;  --  0x2000000
 
    end Initialize_gSPI;
 
    procedure Write_gSPI_Byte (Data : Unsigned_8) is
-      Wifi  : SIO_Peripheral;
+      use RP2350.SIO;
       Temp  : Unsigned_8 := Data;
    begin
       -- Ensure host drives the shared data line
@@ -99,17 +92,18 @@ begin
 
       -- Send MSB First
       for Bit in 1 .. 8 loop
-         Wifi.GPIO_OUT_CLR := Mask_CLK; -- Clock Low
+
+         SIO_Periph.GPIO_OUT_CLR := Mask_CLK; -- Clock Low
          if (Temp and 16#80#) /= 0 then
-            Wifi.GPIO_OUT_SET := Mask_DATA;
+            SIO_Periph.GPIO_OUT_SET := Mask_DATA;
          else
-            Wifi.GPIO_OUT_CLR := Mask_DATA;
+            SIO_Periph.GPIO_OUT_CLR := Mask_DATA;
          end if;
 
          -- Brief delay matching CYW43439 timing constraints (up to 33MHz limit)
          Wait (Microseconds (5));
          -- Clock High (CYW43439 samples on rising edge)
-         Wifi.GPIO_OUT_SET := Mask_CLK;
+         SIO_Periph.GPIO_OUT_SET := Mask_CLK;
          Temp := Shift_Left (Temp, 1);
          Wait (Microseconds (5));
       end loop;
@@ -117,20 +111,19 @@ begin
    end Write_gSPI_Byte;
 
    function Read_gSPI_Byte return Unsigned_8 is
-      use RP2350;
-      Wifi   : SIO_Peripheral;
+      use RP2350.SIO;
       Result : Unsigned_8 := 0;
    begin
       -- Relinquish host drive control so CYW43439 can transmit
-       Wifi.GPIO_OE_CLR := Mask_DATA;
+       SIO_Periph.GPIO_OE_CLR := Mask_DATA;
       for Bit_Num in 1 .. 8 loop
-         Wifi.GPIO_OUT_CLR := Mask_CLK; -- Clock Low
+         SIO_Periph.GPIO_OUT_CLR := Mask_CLK; -- Clock Low
          Wait (Microseconds (5));
-         Wifi.GPIO_OUT_SET := Mask_CLK; -- Clock High
+         SIO_Periph.GPIO_OUT_SET := Mask_CLK; -- Clock High
          --  Shift tracking register to make room for next incoming bit
          Result := Shift_Left (Result, 1);
          --  Capture pin level from hardware input
-         if (Wifi.GPIO_IN and Mask_DATA) /= 0 then
+         if (SIO_Periph.GPIO_IN and Mask_DATA) /= 0 then
             -- Push 1 into LSB of result
             Result := Result or 16#01#;
          end if;
@@ -152,7 +145,8 @@ begin
 
    end Write_gSPI_Word32;
 
-   procedure Set_Onboard_LED (Enable : Boolean) is      
+   procedure Set_Onboard_LED (Enable : Boolean) is
+      use RP2350.SIO;
       -- CYW43439 gSPI Protocol Header definitions:
       -- Bit 31: Write Command (1)
       -- Bit 30: Auto-Increment Address (1)
@@ -171,7 +165,6 @@ begin
                          Shift_Left (1, 26) or             -- Function 1 (Backplane)
                          Shift_Left (GPIO_Out_Addr, 11) or -- Target memory address
                          4;                                -- Length of payload (4 Bytes)
-      Wifi           : SIO_Peripheral;                  
       Payload_Value  : Unsigned_32 := 0;
    begin
       -- Determine payload state for WL_GPIO0
@@ -183,7 +176,7 @@ begin
 
       -- Execute the gSPI bus cycle transaction
        -- Assert Chip Select Low to begin transaction
-      Wifi.GPIO_OUT_CLR  := Mask_CS;
+      SIO_Periph.GPIO_OUT_CLR  := Mask_CS;
       Write_gSPI_Word32 (SPI_Header);    -- Stream Header over SPI line
      
       -- CRITICAL CRUX: The gSPI Turnaround Delay
@@ -193,7 +186,7 @@ begin
       Write_gSPI_Word32 (Payload_Value); -- Stream Data Payload over SPI line
       Wait (Milliseconds (5));
       -- Deassert Chip Select High to conclude transfer
-      Wifi.GPIO_OUT_SET := Mask_CS;
+      SIO_Periph.GPIO_OUT_SET := Mask_CS;
 
    end Set_Onboard_LED;
 
