48236cbc5ba9e8fcc62dd483f1b750f01f53df9f
rogermc2 <rogermc@iinet.net.au>
2026-08-10 17:44:11 +1000

    Update

--- my_code/pico2w_cyw43439_wakeup/src/wakeup.adb
+++ my_code/pico2w_cyw43439_wakeup/src/wakeup.adb
@@ -1,6 +1,4 @@
 
---  with RP2350_CYW43439; use RP2350_CYW43439;
-
 with CYW43_Bitbang; use CYW43_Bitbang;
 with CYW43439_Driver; use CYW43439_Driver;
 with CYW43439_Verification; use CYW43439_Verification;
@@ -8,7 +6,6 @@ with CYW43439_Verification; use CYW43439_Verification;
 procedure Wakeup is
    OK : Boolean;
 begin
-   --  Initialize_gSPI;
    Execute_W_Wakeup;
    OK := Verify_Chip_Communication;
 
